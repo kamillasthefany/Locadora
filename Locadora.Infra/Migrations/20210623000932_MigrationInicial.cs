8360af761eddc95eb7b0375890af4a841a6c87db
Kamilla <kamilla.rodrigues@auvo.com.br>
2021-06-22 21:17:56 -0300

    Presentation com crud funcional

---
+++ Locadora.Infra/Migrations/20210623000932_MigrationInicial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Locadora.Infra.Migrations
+{
+    public partial class MigrationInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Titulo = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Descricao = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UltimaAlteracao = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Deletado = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
---
+++ Locadora.Presentation/Controllers/FilmeController.cs
@@ -0,0 +1,64 @@
+﻿using Locadora.Application.DTOs;
+using Locadora.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locadora.Presentation.Controllers
+{
+    public class FilmeController : Controller
+    {
+        private readonly IApplicationServiceFilme _service;
+
+        public FilmeController(IApplicationServiceFilme applicationService)
+        {
+            _service = applicationService;
+        }
+
+        public IActionResult Index()
+        {
+            var filmes = _service.GetAll();
+            return View(filmes);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            FilmeDTO filme = new FilmeDTO();
+            return View(filme);
+        }
+
+        [HttpPost]
+        public ActionResult Create(FilmeDTO filme)
+        {
+            _service.Add(filme);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var filme = _service.GetById(id);
+            return View(filme);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FilmeDTO filme)
+        {
+            _service.Update(filme);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var filme = _service.GetById(id);
+            return View(filme);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(FilmeDTO filme)
+        {
+            _service.Remove(filme);
+            return RedirectToAction("Index");
+        }
+    }
+}
